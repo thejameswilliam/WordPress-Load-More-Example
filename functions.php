3db33f5875a808044e39d7a33028e141294e6c71
James W. Johnson <me@thejameswilliam.com>
2020-09-16 10:42:18 -0600

    added example code... non functioning without wordpress.

---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+
+wp_register_script('unique-load-more', get_template_directory_uri() . '/loadMore.js', array('jquery'), THEME_VERSION, true);
+wp_enqueue_script('unique-load-more');
+
+
+wp_localize_script( 'unique-load-more', 'unique_loadmore_params', array(
+	'ajaxurl' => site_url() . '/wp-admin/admin-ajax.php', // WordPress AJAX
+	'current_page' => get_query_var( 'paged' ) ? get_query_var('paged') : 1,
+	'post_type' => get_query_var( 'post_type' )
+));
+
+
+
+function unique_loadmore_ajax_handler(){
+	// prepare our arguments for the query
+  $args = array();
+	$args['paged'] = $_POST['page'] + 1; // we need next page to be loaded
+	$args['post_status'] = 'publish';
+  $args['post_type'] = ($_POST['post_type']);
+  $args['tax_query'] = array(
+      'relation' => 'OR',
+      array(
+          'taxonomy' => 'gc_post_type',
+          'field' => 'slug',
+          'terms' => 'voices-from-the-network',
+          'operator' => 'NOT IN'
+      )
+  );
+
+	// it is always better to use WP_Query but not here
+	query_posts( $args );
+
+	if( have_posts() ) :
+		// run the loop
+		while( have_posts() ): the_post();
+			get_template_part( 'loop-post' );
+		endwhile;
+
+	endif;
+	die; // here we exit the script and even no wp_reset_query() required!
+}
+
+
+add_action('wp_ajax_loadmore', 'unique_loadmore_ajax_handler'); // wp_ajax_{action}
+add_action('wp_ajax_nopriv_loadmore', 'unique_loadmore_ajax_handler'); // wp_ajax_nopriv_{action}
---
+++ loadMore.js
@@ -0,0 +1,39 @@
+(function( root, $, undefined ) {
+	"use strict";
+
+	$(function () {
+    //Make sure the .class below matches your load more button's class
+    $(document).on('click', '.load-more-posts', function() {
+      let button = $(this),
+      data = {
+        action: "loadmore",
+        query: unique_loadmore_params.posts, // that's how we get params from wp_localize_script() function
+        page: unique_loadmore_params.current_page,
+        post_type : unique_loadmore_params.post_type
+      };
+      $.ajax({
+        url : unique_loadmore_params.ajaxurl, // AJAX handler
+        data : data,
+        type : 'POST',
+        beforeSend : function ( xhr ) {
+          // change the button text, you can also add a spinner animation here
+          button.text('Loading...');
+        },
+        success : function( data ){
+          if( data ) {
+            //change the button back whatever was there originally (or something new)
+            button.text( 'Load More' );
+            //Change the #id below to the id of the imediate parent container for your posts.
+            $('#posts-container').append(data); // insert new posts
+            unique_loadmore_params.current_page++;
+
+          } else {
+            // if no data, remove the button
+            button.remove();
+          }
+        }
+      });
+    });
+
+  })
+})
